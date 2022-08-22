7ee4454541c0430e68c65f2c8b5d6e1e81efcc07
chackylee <chackylee@msi.com>
2022-08-22 15:46:46 +0800

    注释添加

--- lld-im-service/src/main/java/com/lld/im/service/friendship/model/resp/CheckFriendShipResp.java
+++ lld-im-service/src/main/java/com/lld/im/service/friendship/model/resp/CheckFriendShipResp.java
@@ -30,11 +30,11 @@ public class CheckFriendShipResp {
     private String toId;
 
     //校验状态，根据双向校验和单向校验有不同的status
-    //单向校验：1 from添加了to，不确定to是否添加了from；0from没有添加to，也不确定to有没有添加from
-    //双向校验 1 from添加了to，to也添加了from
-    //        2 from添加了t0，to没有添加from
-    //        3 to添加了from，from没有添加to
-    //        4 双方都没有添加
+    //单向校验：1 from添加了to，不确定to是否添加了from CheckResult_singe_Type_AWithB；0from没有添加to，也不确定to有没有添加from CheckResult_singe_Type_NoRelation
+    //双向校验 1 from添加了to，to也添加了from CheckResult_Type_BothWay
+    //        2 from添加了t0，to没有添加from CheckResult_Both_Type_AWithB
+    //        3 to添加了from，from没有添加to CheckResult_Both_Type_BWithA
+    //        4 双方都没有添加 CheckResult_Both_Type_NoRelation
     private Integer status;
 
 }
