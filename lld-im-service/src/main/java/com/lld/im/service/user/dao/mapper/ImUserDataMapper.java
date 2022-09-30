4114370ce0c8573eebed35ec5c10536e66b6c25f
chackylee <chackylee@msi.com>
2022-09-30 13:16:58 +0800

    小问题完善

--- lld-im-service/src/main/java/com/lld/im/service/user/dao/mapper/ImUserDataMapper.java
+++ lld-im-service/src/main/java/com/lld/im/service/user/dao/mapper/ImUserDataMapper.java
@@ -3,7 +3,6 @@ package com.lld.im.service.user.dao.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.lld.im.service.user.dao.ImUserDataEntity;
 import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
 
 @Mapper
 public interface ImUserDataMapper extends BaseMapper<ImUserDataEntity> {
