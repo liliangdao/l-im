bd6ece7eb07f851b9ebedcbeeed2b00467e1dd69
chackylee <chackylee@msi.com>
2022-09-30 11:06:33 +0800

    类调整

--- lld-im-service/src/main/java/com/lld/im/service/config/EasySqlInjectorProcess.java
+++ lld-im-service/src/main/java/com/lld/im/service/config/EasySqlInjectorProcess.java
@@ -1,9 +1,14 @@
 package com.lld.im.service.config;
 
+import com.baomidou.mybatisplus.core.injector.AbstractMethod;
 import com.baomidou.mybatisplus.core.injector.DefaultSqlInjector;
+import com.baomidou.mybatisplus.extension.injector.methods.InsertBatchSomeColumn;
+import com.github.jeffreyning.mybatisplus.base.MppSqlInjector;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * @description: spring 后置处理器，兼容mybatisplus联合主键插件和批量插入插件
@@ -22,4 +27,19 @@ public class EasySqlInjectorProcess implements BeanPostProcessor {
         return bean;
     }
 
+}
+
+class EasySqlInjector extends MppSqlInjector {
+
+    @Override
+    public List<AbstractMethod> getMethodList(Class<?> mapperClass) {
+
+        if(mapperClass.getName().equals("com.lld.im.service.message.dao.mapper.ImMessageHistoryMapper")){
+            List<AbstractMethod> methodList = super.getMethodList(mapperClass);
+            methodList.add(new InsertBatchSomeColumn());//insertBatchSomeColumn
+            return methodList;
+        }
+
+        return super.getMethodList(mapperClass);
+    }
 }
