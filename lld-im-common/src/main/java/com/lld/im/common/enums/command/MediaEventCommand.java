48aab1c5699b9cb867ab397556a8bc5a934264e8
chackylee <chackylee@msi.com>
2022-12-30 15:22:49 +0800

    command提交

--- lld-im-common/src/main/java/com/lld/im/common/enums/command/MediaEventCommand.java
+++ lld-im-common/src/main/java/com/lld/im/common/enums/command/MediaEventCommand.java
@@ -12,13 +12,13 @@ public enum MediaEventCommand implements Command {
     ACCEPT_CALL(6002),
 
     //6003 同步ice
-    TRANSMIT_ICE(6003),
+//    TRANSMIT_ICE(6003),
 
-    //6004 发送offer
-    TRANSMIT_OFFER(6004),
-
-    //6005 发送ANSWER
-    TRANSMIT_ANSWER(6005),
+//    //6004 发送offer
+//    TRANSMIT_OFFER(6004),
+//
+//    //6005 发送ANSWER
+//    TRANSMIT_ANSWER(6005),
 
     //6006 hangup 挂断
     HANG_UP(6006),
@@ -39,6 +39,7 @@ public enum MediaEventCommand implements Command {
     }
 
 
+    @Override
     public Integer getCommand() {
         return command;
     }
